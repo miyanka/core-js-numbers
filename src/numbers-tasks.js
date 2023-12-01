b06ad15ebe8f380a1a5b44cfc4f289611bf558f2
Yana <bojcenko.jana@gmail.com>
2023-12-01 15:49:09 +0100

    feat: implement isPrime

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -200,8 +200,21 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n === 2 || n === 3) {
+    return true;
+  }
+
+  if (n <= 1 || n % 2 === 0 || n % 3 === 0) {
+    return false;
+  }
+
+  for (let i = 5; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
