4fbe650ab45f1a1c402e9fd1e8d51b57297d29fe
Yana <bojcenko.jana@gmail.com>
2023-12-01 21:14:24 +0100

    feat: implement getSumOfDigits

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -297,8 +297,11 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  return num
+    .toString()
+    .split('')
+    .reduce((sum, current) => sum + +current, 0);
 }
 
 /**
