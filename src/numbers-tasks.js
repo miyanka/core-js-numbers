37d7d3b87164a147b5e429daf45dbeaa84a78a88
Yana <bojcenko.jana@gmail.com>
2023-12-01 22:12:02 +0100

    feat: implement getRandomInteger

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -590,8 +590,8 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(/* min, max */) {
-  throw new Error('Not implemented');
+function getRandomInteger(min, max) {
+  return min + Math.floor(Math.random() * (max + 1 - min));
 }
 
 /**
